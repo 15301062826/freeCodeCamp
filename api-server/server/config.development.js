fc131944b0aba3ceea60109cff690a9a102794c9
15301062826 <57606708+15301062826@users.noreply.github.com>
2020-12-25 10:14:49 +0800

    Update config.development.js

--- api-server/server/config.development.js
+++ api-server/server/config.development.js
@@ -7,3 +7,21 @@ module.exports = {
     clientSecret: process.env.GITHUB_SECRET
   }
 };
+module.exports = {
+  host: '127.0.0.1',
+  sessionSecret: process.env.SESSION_SECRET,
+
+  github: {
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
+  }
+};
+module.exports = {
+  host: '127.0.0.1',
+  sessionSecret: process.env.SESSION_SECRET,
+
+  github: {
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
+  }
+};
